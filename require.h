d241b6857cfddb77a845b3c586cbf9c953c8e452
Tim Towtdi <tim.towtdi@mail.fake>
2013-02-08 03:51:20 +0300

    New file extension added for ignore. require.h updated with code from book.

--- require.h
+++ require.h
@@ -1,70 +1,58 @@
 //: :require.h
-// From "Thinking in C++, Volume 2", by Bruce Eckel & Chuck Allison.
-// (c) 1995-2004 MindView, Inc. All Rights Reserved.
-// See source code use permissions stated in the file 'License.txt',
-// distributed with the code package available at www.MindView.net.
-// Test for error conditions in programs.
+// Проверка ошибок в программах
+// Локальные "using namespace std" для старых компиляторов
 #ifndef REQUIRE_H
 #define REQUIRE_H
 #include <cstdio>
 #include <cstdlib>
 #include <fstream>
+#include <string>
 
-inline void require(bool requirement,
-  const char* msg = "Requirement failed") {
-  // Local "using namespace std" for old compilers:
-  using namespace std;
-  if(!requirement) {
-    fputs(msg, stderr);
-    fputs("\n", stderr);
-    exit(EXIT_FAILURE);
-  }
+inline void require(bool requirement, 
+	const std::string& msg = "Requirement failed") {
+	using namespace std;
+	if (!requirement) {
+		fputs(msg.c_str(), stderr);
+		fputs("\n", stderr);
+		exit(1);
+	}
 }
 
 inline void requireArgs(int argc, int args,
-  const char* msg = "Must use %d arguments") {
-  using namespace std;
-  if(argc != args + 1) {
-    fprintf(stderr, msg, args);
-    fputs("\n", stderr);
-    exit(EXIT_FAILURE);
-  }
+	const std::string& msg = "Must use %d arguments") {
+	using namespace std;
+	if (argc != args + 1) {
+		fprintf(stderr, msg.c_str(), args);
+		fputs("\n", stderr);
+		exit(1);
+	}
 }
 
 inline void requireMinArgs(int argc, int minArgs,
-  const char* msg = "Must use at least %d arguments") {
-  using namespace std;
-  if(argc < minArgs + 1) {
-    fprintf(stderr, msg, minArgs);
-    fputs("\n", stderr);
-    exit(EXIT_FAILURE);
-  }
+	const std::string& msg = "Must use at least %d arguments") {
+	using namespace std;
+	if (argc < minArgs + 1) {
+		fprintf(stderr, msg.c_str(), minArgs);
+		fputs("\n", stderr);
+		exit(1);
+	}
 }
 
-inline void assure(std::ifstream& in,
-  const char* filename = "") {
-  using namespace std;
-  if(!in) {
-    fprintf(stderr, "Could not open file %s\n", filename);
-    exit(EXIT_FAILURE);
-  }
+inline void assure(std::ifstream& in, 
+	const std::string& filename = "") {
+	using namespace std;
+	if (!in) {
+		fprintf(stderr, "Could not open file %s\n", filename.c_str());
+		exit(1);
+	}
 }
 
-inline void assure(std::ofstream& in,
-  const char* filename = "") {
-  using namespace std;
-  if(!in) {
-    fprintf(stderr, "Could not open file %s\n", filename);
-    exit(EXIT_FAILURE);
-  }
+inline void assure(std::ofstream& out, const std::string& filename = "") {
+	using namespace std;
+	if (!out) {
+		fprintf(stderr, "Could not open file %s\n", filename.c_str());
+		exit(1);
+	}
 }
 
-inline void assure(std::fstream& in,
-  const char* filename = "") {
-  using namespace std;
-  if(!in) {
-    fprintf(stderr, "Could not open file %s\n", filename);
-    exit(EXIT_FAILURE);
-  }
-}
-#endif // REQUIRE_H ///:~
+#endif	// REQUIRE_H ///:~
